ba66182517b822208fd533c4451def3b06053aff
James Glanville <james.n.glanville@gmail.com>
2012-02-21 09:01:22 +0000

    more dummy stuff

--- main.cc
+++ main.cc
@@ -6,6 +6,13 @@ int main ()
 {
 	if (Comms.init() != 0) {return -1;}
 	
+	while (1)
+	{
+		//poll everything
+		//decide if we need to jump to next state
+		//decide based on state what to do
+		//do it
+	}	
 
 }
 
